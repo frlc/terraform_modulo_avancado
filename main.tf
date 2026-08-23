e6cd8d89c3712719db486e27889878a25043e242
Fernando Cruz <frlc_31@hotmail.com>
2026-08-22 21:25:45 -0300

    teste de foreach

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "this" {
   for_each = var.instancias
-  count         = var.env == "prod" ? 1 : 0
+  #count         = var.env == "prod" ? 1 : 0
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
