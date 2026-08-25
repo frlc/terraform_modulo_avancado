6def612aa9842aa7f7b13ef90dca1805b390eee1
Fernando Cruz <frlc_31@hotmail.com>
2026-08-24 23:01:34 -0300

    loop

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "instance_ip" {
   description = "Ip privado da instância"
-  value       = try(aws_instance.example[0].private_ip, null)
+  value       = try(aws_instance.this[0].private_ip, null)
 }
